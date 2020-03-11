dababcbb1a1dbaed123ad8a8038129f2dbb63c0d
xtaci <daniel820313@gmail.com>
2020-03-11 20:35:29 +0800

    adjust duration

--- consensus/consensus.go
+++ consensus/consensus.go
@@ -345,7 +345,7 @@ func (c *Consensus) init(config *Config) {
 
 //  calculates roundchangeDuration
 func (c *Consensus) roundchangeDuration(round uint64) time.Duration {
-	return 2 * time.Duration(round+1) * c.latency
+	return time.Duration(round+1) * c.latency
 }
 
 //  calculates collectDuration
@@ -365,7 +365,7 @@ func (c *Consensus) commitDuration(round uint64) time.Duration {
 
 // calculates lockReleaseDuration
 func (c *Consensus) lockReleaseDuration(round uint64) time.Duration {
-	return 2 * time.Duration(round+1) * c.latency
+	return time.Duration(round+1) * c.latency
 }
 
 // maximalLocked finds the maximum locked data in this round,
