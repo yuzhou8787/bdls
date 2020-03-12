2c8a96a1f17ac38f75b6e403f67bd92587910dd4
xtaci <daniel820313@gmail.com>
2020-03-12 20:22:22 +0800

    update pkg comment

--- consensus/doc.go
+++ consensus/doc.go
@@ -1,3 +1,12 @@
-// Package consensus implements deterministic consensus algorithm based on
-// https://eprint.iacr.org/2019/1460.pdf
+/*	Package consensus implements Sperax Byzantine Fault Tolerance in Partially Connected Asynchronous
+	Networks based on https://eprint.iacr.org/2019/1460.pdf.
+
+	To make the runtime behavior of consensus algorithm predictable, as a function:
+	y = f(x, t), where 'x' is the message it received, and 't' is the time while being called,
+	this library has been designed in a deterministic scheme, without parallel
+	computing, networking, and even current time is a parameter to this library.
+
+	As it's a pure algorithm implementation, it's not thread-safe then! Users of this library
+	should take care of their own sychronziation mechanism.
+*/
 package consensus
