68aec0ef7beb511be34d909da06b27dfa54aae1c
xtaci <daniel820313@gmail.com>
2020-04-09 18:12:25 +0800

    init add a consensus emulator

---
+++ cmd/emucon/main.go
@@ -0,0 +1,289 @@
+// BSD 3-Clause License
+//
+// Copyright (c) 2020, Sperax
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//
+// 1. Redistributions of source code must retain the above copyright notice, this
+//    list of conditions and the following disclaimer.
+//
+// 2. Redistributions in binary form must reproduce the above copyright notice,
+//    this list of conditions and the following disclaimer in the documentation
+//    and/or other materials provided with the distribution.
+//
+// 3. Neither the name of the copyright holder nor the names of its
+//    contributors may be used to endorse or promote products derived from
+//    this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+// DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+// FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+// DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+// SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+// CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+// OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+// OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+package main
+
+import (
+	"bytes"
+	"crypto/ecdsa"
+	"crypto/rand"
+	"encoding/hex"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io"
+	"log"
+	"math/big"
+	"net"
+	"os"
+	"time"
+
+	"github.com/Sperax/bdls"
+	"github.com/Sperax/bdls/agent-tcp"
+	"github.com/Sperax/bdls/crypto/blake2b"
+	"github.com/urfave/cli/v2"
+)
+
+// A quorum set for consenus
+type Quorum struct {
+	Keys []*big.Int `json:"keys"` // pem formatted keys
+}
+
+func main() {
+	app := &cli.App{
+		Name:                 "BDLS consensus protocol emulator",
+		Usage:                "Act as a real participant in BDLS consensus procedure via TCP",
+		EnableBashCompletion: true,
+		Commands: []*cli.Command{
+			{
+				Name:  "genkeys",
+				Usage: "generate identies group to participant in consensus",
+				Flags: []cli.Flag{
+					&cli.IntFlag{
+						Name:  "count",
+						Value: 4,
+						Usage: "number of participant to generate",
+					},
+					&cli.StringFlag{
+						Name:  "config",
+						Value: "./quorum.json",
+						Usage: "output quorum file, choose one(number) as a participant to use play the role",
+					},
+				},
+				Action: func(c *cli.Context) error {
+					count := c.Int("count")
+					quorum := &Quorum{}
+					// generate private keys
+					for i := 0; i < count; i++ {
+						privateKey, err := ecdsa.GenerateKey(bdls.DefaultCurve, rand.Reader)
+						if err != nil {
+							return err
+						}
+
+						quorum.Keys = append(quorum.Keys, privateKey.D)
+					}
+
+					file, err := os.Create(c.String("config"))
+					if err != nil {
+						return err
+					}
+					enc := json.NewEncoder(file)
+					enc.SetIndent("", "\t")
+					err = enc.Encode(quorum)
+					if err != nil {
+						return err
+					}
+					file.Close()
+
+					log.Println("generate", c.Int("count"), "keys")
+					return nil
+				},
+			},
+			{
+				Name:  "run",
+				Usage: "start a consensus agent",
+				Flags: []cli.Flag{
+					&cli.StringFlag{
+						Name:  "listen",
+						Value: ":4680",
+						Usage: "the client's listening port",
+					},
+					&cli.IntFlag{
+						Name:  "id",
+						Value: 0,
+						Usage: "the node id, will use the n-th private key in quorum.json",
+					},
+					&cli.StringFlag{
+						Name:  "config",
+						Value: "./quorum.json",
+						Usage: "the shared quorum config file",
+					},
+					&cli.StringFlag{
+						Name:  "peers",
+						Value: "./peers.json",
+						Usage: "the peers ip list to connect",
+					},
+				},
+				Action: func(c *cli.Context) error {
+					// open quorum config
+					file, err := os.Open(c.String("config"))
+					if err != nil {
+						return err
+					}
+					defer file.Close()
+
+					quorum := new(Quorum)
+					err = json.NewDecoder(file).Decode(quorum)
+					if err != nil {
+						return err
+					}
+
+					id := c.Int("id")
+					if id >= len(quorum.Keys) {
+						return errors.New(fmt.Sprint("cannot locate private key for id:", id))
+					}
+
+					// create configuration
+					config := new(bdls.Config)
+					config.Epoch = time.Now()
+					config.CurrentState = make([]byte, 1024) // fake state
+					config.CurrentHeight = 0
+					config.StateCompare = func(a bdls.State, b bdls.State) int { return bytes.Compare(a, b) }
+					config.StateValidate = func(bdls.State) bool { return true }
+
+					for k := range quorum.Keys {
+						priv := new(ecdsa.PrivateKey)
+						priv.PublicKey.Curve = bdls.DefaultCurve
+						priv.D = quorum.Keys[k]
+						priv.PublicKey.X, priv.PublicKey.Y = bdls.DefaultCurve.ScalarBaseMult(priv.D.Bytes())
+						// myself
+						if id == k {
+							config.PrivateKey = priv
+						}
+
+						// set validator sequence
+						config.Participants = append(config.Participants, &priv.PublicKey)
+					}
+
+					// create consensus
+					consensus, err := bdls.NewConsensus(config)
+					if err != nil {
+						return err
+					}
+					consensus.SetLatency(200 * time.Millisecond)
+
+					// load endpoints
+					file, err = os.Open(c.String("peers"))
+					if err != nil {
+						return err
+					}
+					defer file.Close()
+
+					var peers []string
+					err = json.NewDecoder(file).Decode(&peers)
+					if err != nil {
+						return err
+					}
+
+					// start listener
+					tcpaddr, err := net.ResolveTCPAddr("tcp", c.String("listen"))
+					if err != nil {
+						return err
+					}
+
+					l, err := net.ListenTCP("tcp", tcpaddr)
+					if err != nil {
+						return err
+					}
+					log.Println("listening on:", tcpaddr)
+					log.Println("identity:", id)
+
+					// initiate tcp agent
+					tagent := agent.NewTCPAgent(consensus, config.PrivateKey)
+					if err != nil {
+						return err
+					}
+
+					// start updater
+					tagent.Update()
+
+					// passive connection from peers
+					go func() {
+						for {
+							conn, err := l.Accept()
+							if err != nil {
+								panic(err)
+							}
+							log.Println("passive peer:", conn.RemoteAddr())
+							// peer endpoint created
+							p := agent.NewTCPPeer(conn, tagent)
+							tagent.AddPeer(p)
+							// prove my identity to this peer
+							p.InitiatePublicKeyAuthentication()
+						}
+					}()
+
+					// active connections to peers
+					for k := range peers {
+						go func(raddr string) {
+							for {
+								conn, err := net.Dial("tcp", raddr)
+								if err == nil {
+									log.Println("active peer:", conn.RemoteAddr())
+									// peer endpoint created
+									p := agent.NewTCPPeer(conn, tagent)
+									tagent.AddPeer(p)
+									// prove my identity to this peer
+									p.InitiatePublicKeyAuthentication()
+									return
+								}
+
+								log.Println(err)
+								<-time.After(time.Second)
+							}
+						}(peers[k])
+					}
+
+					var height uint64
+				PROPOSE:
+					for {
+						data := make([]byte, 1024)
+						io.ReadFull(rand.Reader, data)
+						tagent.Propose(data)
+
+						for {
+							newHeight, newRound, newState := tagent.GetLatestState()
+							if newHeight > height {
+								now := time.Now()
+								h := blake2b.Sum256(newState)
+								log.Printf("%v <decide> at height:%v round:%v hash:%v", now.Format("15:04:05"), newHeight, newRound, hex.EncodeToString(h[:]))
+								height = newHeight
+								continue PROPOSE
+							}
+							// wait
+							<-time.After(20 * time.Millisecond)
+						}
+					}
+				},
+			},
+		},
+
+		Action: func(c *cli.Context) error {
+			cli.ShowAppHelp(c)
+			return nil
+		},
+	}
+
+	err := app.Run(os.Args)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+}
